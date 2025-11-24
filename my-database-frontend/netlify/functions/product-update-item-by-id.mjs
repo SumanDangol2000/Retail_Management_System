57317e05bc138749a71b9751de1159035b428eed
Suman Dangol <sdangol368@gmial.com>
2025-11-24 14:12:34 -0600

    create product update API and its logic

---
+++ my-database-frontend/netlify/functions/product-update-item-by-id.mjs
@@ -0,0 +1,77 @@
+import { neon } from '@neondatabase/serverless';
+
+export default async (req, context) => {
+  try {
+
+    // Only allow UPDATE
+    if (req.method !== "PUT") {
+      return new Response(JSON.stringify({ error: "Method not allowed" }), {
+        status: 405,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    // Parse query parameters
+    const url = new URL(req.url);
+    const id = url.searchParams.get('id');
+
+    if (!id) {
+      return new Response(
+        JSON.stringify({ error: 'ID parameter required' }),
+        { status: 400, headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    // SAFE JSON PARSING
+    let body = {};
+    try {
+      body = await req.json();
+    } catch (err) {
+      console.error("JSON PARSE ERROR:", err);
+      return new Response(JSON.stringify({ error: "Invalid JSON" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    const { product_name, category_id, supplier_id, price, quantity } = body;
+
+            // Validate input
+    if (!product_name || !category_id || !supplier_id || !price || !quantity) {
+      return new Response(
+        JSON.stringify({ error: 'Required fields missing' }),
+        { status: 400, headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    const sql = neon(process.env.DATABASE_URL);
+
+    // Parameterized query (prevents SQL injection)
+    const result = await sql`UPDATE Products 
+                             SET product_name = ${product_name}, category_id = ${category_id}, supplier_id = ${supplier_id}, price = ${price}, quantity_in_stock = ${quantity} 
+                             WHERE product_id = ${id} RETURNING *;`;
+
+    // return new Response(JSON.stringify(result[0] || {}), {
+    //   status: result.length > 0 ? 200 : 404,
+    //   headers: { 'Content-Type': 'application/json' }
+    // });
+
+    if (result.length === 0) {
+      return new Response(JSON.stringify({ error: "Record not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    return new Response(JSON.stringify(result[0]), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+};
